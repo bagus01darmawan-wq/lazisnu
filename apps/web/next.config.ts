a01b5c9a7038228c34b47f68c7a42a44e9a744d5
Bagus Darmawan <bagus01darmawan@gmail.com>
2026-06-16 19:37:37 +0000

    chore(web): update sentry org and project slugs
    Sesuaikan dengan nilai asli di dashboard Sentry agar upload source map berhasil.

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -8,8 +8,8 @@ const nextConfig: NextConfig = {
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
-  org: "lazisnu",
-  project: "lazisnu-web",
+  org: "lazisnupng",
+  project: "javascript-nextjs",
   silent: !process.env.CI,
   widenClientFileUpload: true,
   tunnelRoute: "/monitoring",
