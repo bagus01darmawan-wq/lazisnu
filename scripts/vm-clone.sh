093cae710daa41809e9cf7e4d4c315e8246c2221
Bagus Darmwan <bagus01darmawan@gmail.com>
2026-07-23 11:57:31 +0700

    chore: remove sensitive files from repo

--- scripts/vm-clone.sh
+++
@@ -1,6 +0,0 @@
-apt-get update && apt-get install -y git
-cd /opt && rm -rf lazisnu
-git clone https://github.com/bagus01darmawan-wq/lazisnu.git lazisnu
-cd lazisnu
-git checkout feature/sync-sprint-cleanup
-ls
